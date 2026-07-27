c8e595ff39ff6e6383646384288313784492b365
SURYAVANSHI UMA <suryavanshiu67@gmail.com>
2026-07-27 12:42:14 +0530

    uploaded SQL query files

---
+++ SQL Scripts/data_file.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE PizzaBase;
+
+CREATE TABLE orders (
+order_id int not null,
+order_date date not null,
+order_time time not null,
+primary key(order_id) 
+);
+
+CREATE TABLE order_details (
+order_details_id int not null,
+order_id int not null,
+pizza_id text not null,
+quantity int not null,
+primary key(order_details_id)
+);
+
+
+
